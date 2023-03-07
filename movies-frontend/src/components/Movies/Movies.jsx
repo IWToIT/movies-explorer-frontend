32e89520127348d5aa7d7220aba2b70ace8a19ea
unknown <awesomeman4455@jandex.ru>
2023-03-07 23:47:28 +0100

    ревью 1

--- movies-frontend/src/components/Movies/Movies.jsx
+++ movies-frontend/src/components/Movies/Movies.jsx
@@ -1,13 +1,46 @@
-import MoviesCardList from "../MoviesCardList/MoviesCardList";
-/* import Preloader from "../Preloader/Preloader"; реализовать когда будет подключен API*/
-import SearchForm from "../SearchForm/SearchForm";
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import SearchForm from '../SearchForm/SearchForm';
 
-function Movies({ location }) {
+function Movies({
+  isBlockingButton,
+  allMovieslist,
+  showMovies,
+  filterMovies,
+  savedMovies,
+  resStatus,
+  isLoadingMovies,
+  location,
+  formValues,
+  onSearchFilms,
+  onMovieLike,
+  onButtonMore,
+  checkboxFilter,
+}) {
   return (
     <section className="movies">
-      <SearchForm />
-      {/* <Preloader /> реализовать когда будет подключен API */}
-      <MoviesCardList location={location} />
+      <SearchForm
+        isBlockingButton={isBlockingButton}
+        checkboxFilter={checkboxFilter}
+        formValues={formValues}
+        location={location}
+        onSearchFilms={onSearchFilms}
+      />
+      <p className="movies__error-message">
+        {!allMovieslist.length
+          ? 'После поиска здесь будет отображен список фильмов.'
+          : !filterMovies.length && 'Ничего не найдено.'}
+      </p>
+      <MoviesCardList
+        isBlockingButton={isBlockingButton}
+        showMovies={showMovies}
+        onButtonMore={onButtonMore}
+        onMovieLike={onMovieLike}
+        resStatus={resStatus}
+        isLoadingMovies={isLoadingMovies}
+        filterMovies={filterMovies}
+        savedMovies={savedMovies}
+        location={location}
+      />
     </section>
   );
 }
--- movies-frontend/src/components/SearchForm/SearchForm.jsx
+++ movies-frontend/src/components/SearchForm/SearchForm.jsx
@@ -1,13 +1,66 @@
-function SearchForm() {
+import { useEffect, useState } from 'react';
+
+function SearchForm({ isBlockingButton, formValues, checkboxFilter, onSearchSavedFilms, location, onSearchFilms }) {
+  const [isInvalid, setInvalid] = useState(false);
+  const [searchValue, setSearchValue] = useState(location.pathname === '/movies' ? formValues.value : '');
+  const [searchCheckbox, setCheckbox] = useState(location.pathname === '/movies' ? formValues.checkbox : false);
+
+  useEffect(() => {
+    if (location.pathname === '/movies') {
+      setSearchValue(formValues.value);
+      setCheckbox(formValues.checkbox);
+    }
+  }, [location, formValues]);
+
+  function handleValue(e) {
+    setSearchValue(e.target.value);
+  }
+
+  function handleCheckbox(e) {
+    const checkboxValue = e.target.checked;
+    setCheckbox(checkboxValue);
+    checkboxFilter(searchValue, checkboxValue);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // учитывать пустую строку поиска только на вкладке фильмов
+    if (e.target[0].value === '' && location.pathname === '/movies') {
+      setInvalid(true);
+      return;
+    }
+
+    setInvalid(false);
+
+    if (location.pathname === '/movies') {
+      onSearchFilms(searchValue, searchCheckbox);
+    } else {
+      onSearchSavedFilms(searchValue, searchCheckbox);
+    }
+  }
+
   return (
-    <form className="search-form">
-      <input className="search-form__input" type="text" name="search" placeholder="Фильм" />
-      <button className="search-form__submit" type="submit">
-        <img src={require('../../images/find.png')} alt="Кнопка поиска" className="search-form__image" />
-      </button>
+    <form onSubmit={handleSubmit} className="search-form">
+      <input
+        value={searchValue}
+        onChange={handleValue}
+        className="search-form__input"
+        type="text"
+        name="search"
+        placeholder="Фильм"
+      />
+      <span className="search-form__error">{isInvalid && 'Нужно ввести ключевое слово'}</span>
+      <button disabled={isBlockingButton} className="search-form__submit" type="submit"></button>
       <label htmlFor="search-film" className="search-form__label">
         <span className="search-form__checkbox-text">Короткометражки</span>
-        <input required id="search-film" className="search-form__checkbox" type="checkbox" />
+        <input
+          checked={searchCheckbox}
+          onChange={handleCheckbox}
+          id="search-film"
+          className="search-form__checkbox"
+          type="checkbox"
+        />
       </label>
     </form>
   );
